1c8458723306c9447c1d79621c2ecaabad52908a
Luislag <71040926+Luislag@users.noreply.github.com>
2025-02-11 18:53:38 +0100

    Added dictionary exercises

--- Session_6.py
+++ Session_6.py
@@ -8,8 +8,11 @@ def find_3_letter_words (filename) :
             for p in punctuation_remove:
                 line = line.replace(p, "")
             for p in punctuation_space:
-                line = line.replace(p, "")
+                line = line.replace(p, " ")
             words = line.split()
+            for word in words:
+                if len(word) == 3 and word[0].lower() == "b":
+                    print(word)
 
 
 find_3_letter_words("input.txt")
